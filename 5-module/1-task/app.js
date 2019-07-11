77482d8f6baf11314a2d69efb7bbc474e3890bce
IngvarVs <trueigvs@gmail.com>
2019-07-11 20:39:58 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,34 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const clients = new Set();
+
+
 router.get('/subscribe', async (ctx, next) => {
+    const message = await new Promise( resolve => {
+        clients.add(resolve);
+
+        ctx.res.on('close', () => {
+            if (ctx.res.finished) return;
+            clients.delete(resolve);
+            resolve();
+        })
+    });
+
+    ctx.body = message;
+
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message;
+    if (!message) return next();
+
+    for (const client of clients) {
+        client(message);
+    }
+    clients.clear();
+    ctx.body = 'ok';
+ 
 });
 
 app.use(router.routes());
