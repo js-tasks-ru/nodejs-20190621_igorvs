14f28f57bf8b4b82c4e3b9923434e0b6a84a971c
IngvarVs <trueigvs@gmail.com>
2019-07-14 20:36:36 +0300

    add solution for 6-module 1-task

--- 6-module/1-task/models/Category.js
+++ 6-module/1-task/models/Category.js
@@ -2,10 +2,29 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+   // required: "Укажите подкатегорию",
+    required: true,
+    unique: true,
+  },
 });
 
+
+//const SubCat = mongoose.model("SubCat", subCategorySchema);
+
 const categorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    //required: "Укажите категорию",
+    required: true,
+    unique: true,
+  },
   subcategories: [subCategorySchema]
+  /*subcategories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SubCat",
+  }]*/
 });
 
 module.exports = connection.model('Category', categorySchema);
